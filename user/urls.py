1d7d626f9838959f0f3a2dd9f3f57f23e5e86d04
ankush-at <ankushthakre1995@gmail.com>
2021-03-31 11:36:24 +0530

    first commit

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from user import views
+
+
+#from django.contrib.auth import views as auth_views
+urlpatterns = [
+	path('',views.home),
+	path('addtocart/<int:pk>/',views.addtocart,name='addtocart'),
+	path('cart/',views.cart,name='cart'),
+	path('delete/<int:pk>/',views.delete,name='delete'),
+	path('order/',views.order,name='order'),
+
+]
